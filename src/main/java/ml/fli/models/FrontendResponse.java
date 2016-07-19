1e8834601b2df8412d0190c8ef8ee1b8a559ddef
Кирилл Васин <kirill.vasin@codeinside.ru>
2016-07-19 10:56:58 +0300

    mock controller

--- src/main/java/ml/fli/models/FrontendResponse.java
+++ src/main/java/ml/fli/models/FrontendResponse.java
@@ -1,4 +1,59 @@
 package ml.fli.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FrontendResponse {
+    private Set<Raw> result = new HashSet<>();
+
+    public Set<Raw> getResult() {
+        return result;
+    }
+
+    public void setResult(Set<Raw> result) {
+        this.result = result;
+    }
+
+    /**
+     * Представляет собой одну строку
+     * таблицы результатов
+     */
+    public static class Raw {
+        private String photoUrl;
+        private String accountUrl;
+        private String name;
+        private double rate;
+
+        public String getPhotoUrl() {
+            return photoUrl;
+        }
+
+        public void setPhotoUrl(String photoUrl) {
+            this.photoUrl = photoUrl;
+        }
+
+        public String getAccountUrl() {
+            return accountUrl;
+        }
+
+        public void setAccountUrl(String accountUrl) {
+            this.accountUrl = accountUrl;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getRate() {
+            return rate;
+        }
+
+        public void setRate(double rate) {
+            this.rate = rate;
+        }
+    }
 }
--- src/main/java/ml/fli/services/MockUsersServiceImpl.java
+++ src/main/java/ml/fli/services/MockUsersServiceImpl.java
@@ -5,11 +5,47 @@ import ml.fli.models.FrontendResponse;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
+import java.util.Set;
 
 @Service
 public class MockUsersServiceImpl implements UsersService {
     @Override
     public FrontendResponse get(@Nonnull FrontendRequest request) {
-        return new FrontendResponse();
+        return getSomeUsers();
+    }
+
+    public FrontendResponse getSomeUsers() {
+        FrontendResponse result = new FrontendResponse();
+        Set<FrontendResponse.Raw> raws = result.getResult();
+
+        FrontendResponse.Raw first = new FrontendResponse.Raw();
+        first.setPhotoUrl("https://pp.vk.me/c608629/v608629069/10ca/GcoqLLRzGo4.jpg");
+        first.setAccountUrl("https://vk.com/id186928069");
+        first.setName("Марк Горшков");
+        first.setRate(0.0);
+        raws.add(first);
+
+        FrontendResponse.Raw second = new FrontendResponse.Raw();
+        second.setPhotoUrl("https://pp.vk.me/c626120/v626120705/20501/kLe17uoUAG4.jpg");
+        second.setAccountUrl("https://vk.com/id269611705");
+        second.setName("Алексей Царапкин");
+        second.setRate(0.1);
+        raws.add(second);
+
+        FrontendResponse.Raw third = new FrontendResponse.Raw();
+        third.setPhotoUrl("https://pp.vk.me/c631929/v631929744/1e8ab/rz3_QHhI86E.jpg");
+        third.setAccountUrl("https://vk.com/id347750744");
+        third.setName("Виталий Горышев");
+        third.setRate(0.3);
+        raws.add(third);
+
+        FrontendResponse.Raw forth = new FrontendResponse.Raw();
+        forth.setPhotoUrl("https://pp.vk.me/c626119/v626119383/2899/kkAF3WOwEc0.jpg");
+        forth.setAccountUrl("https://vk.com/sweetkina");
+        forth.setName("Лена Ленская");
+        forth.setRate(0.9);
+        raws.add(forth);
+
+        return result;
     }
 }
