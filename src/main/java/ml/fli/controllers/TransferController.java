51c9d6016daed90b3b300afe3d266bb6d8fe6c59
Ermakov <Eramen@mail.ru>
2016-07-21 12:43:03 +0300

    Merge from master. Delete unused class. Test websockets

--- src/main/java/ml/fli/controllers/TransferController.java
+++ src/main/java/ml/fli/controllers/TransferController.java
@@ -2,10 +2,12 @@ package ml.fli.controllers;
 
 import ml.fli.models.FrontendRequest;
 import ml.fli.models.FrontendResponse;
-import ml.fli.models.FrontendResponseOneUser;
 import ml.fli.models.User;
+import ml.fli.services.MockUsersServiceImpl;
+import ml.fli.services.UsersService;
 import ml.fli.utils.JSONParser;
 import ml.fli.utils.VkApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -15,11 +17,14 @@ import java.util.ArrayList;
 @Controller
 public class TransferController {
 
+    @Autowired
+    private UsersService usersService;
+
     @MessageMapping("/get")
     @SendTo("/data/userList")
     public FrontendResponse get(FrontendRequest message) throws Exception {
         Thread.sleep(30000);
-        String userId = message.getUserId();
+/*        String userId = message.getUserId();
         String choiceSex = message.getSex();
 
         VkApi vkApi = new VkApi();
@@ -27,7 +32,7 @@ public class TransferController {
 
         JSONParser parser = new JSONParser();
         User oneUser = parser.parseUser(resultOneUser);
-//
+//*/
 //        VkApiParams param = VkApiParams.create();
 //        if (oneUser.getCity() instanceof String) {
 //            param.add("city", oneUser.getCity());
@@ -41,16 +46,12 @@ public class TransferController {
 //            }
 //        }
 
-        String userUrl = "vk.com/id" + userId;
+/*        String userUrl = "vk.com/id" + userId;
         String userName = oneUser.getFirst_name() + " " + oneUser.getLast_name();
         String userPic = "https://pp.vk.me/c633221/v633221362/39410/MT0d_XiMpqs.jpg";
-        int userWeight = 10;
+        int userWeight = 10;*/
 
-        FrontendResponseOneUser result = new FrontendResponseOneUser(userUrl, userName, userPic, String.valueOf(userWeight));
-        ArrayList<FrontendResponseOneUser> userList = new ArrayList<>();
-        userList.add(result);
-
-        return new FrontendResponse(userList);
+        return ((MockUsersServiceImpl) usersService).getSomeUsers();
     }
 
 }
