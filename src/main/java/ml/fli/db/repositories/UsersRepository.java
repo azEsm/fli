8d206d06ac996e5e51d3c8e4ae81605b606cb53d
Кирилл Васин <kirill.vasin@codeinside.ru>
2016-07-28 15:55:47 +0300

    hotfix

--- src/main/java/ml/fli/db/repositories/UsersRepository.java
+++ src/main/java/ml/fli/db/repositories/UsersRepository.java
@@ -1,7 +1,6 @@
 package ml.fli.db.repositories;
 
-import ml.fli.db.models.User;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 
-public interface UsersRepository extends CrudRepository<User, Long> {
+public interface UsersRepository /*extends CrudRepository<User, Long>*/ {
 }
